c8f077729baefec7e594959683c5ea1bb3d4ab75
Brian Dunn and Joshua Davey <dev+briandunn+jgdavey@hashrocket.com>
2013-03-04 18:30:36 -0600

    draw image with core graphics

--- main.rb
+++ main.rb
@@ -19,6 +19,13 @@ Imgur = Struct.new(:json) do
   end
 end
 
+class NSImage
+  def CGImage
+    source = CGImageSourceCreateWithData(self.TIFFRepresentation, nil)
+    CGImageSourceCreateImageAtIndex(source, 0, nil)
+  end
+end
+
 NSApplication.sharedApplication
 
 def window
@@ -29,22 +36,32 @@ def window
   end
 end
 
-def image_view
-  funnies = JSON.parse Net::HTTP.get URI 'http://imgur.com/r/funny.json'
-  image = Imgur.new funnies["data"].sample
-  ns_image = NSImage.alloc.initWithContentsOfURL NSURL.alloc.initWithString p image.url
-  imageview = NSImageView.alloc.initWithFrame window.frame
-  imageview.setImage ns_image
-  imageview
+def ns_image
+  @ns_image ||= begin
+    funnies = JSON.parse Net::HTTP.get URI 'http://imgur.com/r/funny.json'
+    image = Imgur.new funnies["data"].sample
+    NSImage.alloc.initWithContentsOfURL NSURL.alloc.initWithString p image.url
+  end
 end
 
-window.contentView.addSubview image_view
-window.contentView.setNeedsDisplay true
-window.makeKeyAndOrderFront nil
+def main_view
+  @main_view ||= window.contentView
+end
 
-# window.contentView.setWantsLayer true
-# layer = CALayer.layer
-# window.contentView.setLayer layer
+def image_layer
+  CALayer.layer.tap do |layer|
+    layer.setContents ns_image.CGImage
+    layer.setBounds CGRectMake(0.0, 0.0, ns_image.size.width, ns_image.size.height)
+    layer.setPosition CGPointMake(ns_image.size.width, ns_image.size.height)
+  end
+end
+
+main_view.setWantsLayer true
+layer = CALayer.layer
+main_view.setLayer layer
+
+layer.addSublayer image_layer
+layer.setNeedsDisplay
 
 # p "window's view: ", window.contentView
 # p "window's view: ", window.contentView.frame
@@ -54,6 +71,7 @@ window.makeKeyAndOrderFront nil
 # layer.opaque = true
 # window.display
 # window.contentView.setDelegate self
+window.makeKeyAndOrderFront nil
 sleep 1
 
 
@@ -68,3 +86,40 @@ sleep 1
 #   window.setFrame unicornRect, display: true, animate: false
 #   window.makeKeyAndOrderFront nil
 # end
+#
+__END__
+    [super viewDidLoad];
+    
+    {   // Create a path to animate a layer on. We will also draw the path.
+        _path = CGPathCreateMutable();
+        CGPoint origin = CGPointMake(-30.0, 480.0);
+        CGPoint destination = CGPointMake(350.0, origin.y);
+        CGFloat midpoint = (destination.x + origin.x) / 2.0;
+        CGFloat peak = 350.0;
+        CGPathMoveToPoint(_path, NULL, origin.x, origin.y);
+        
+        CGPathAddCurveToPoint(_path, NULL, midpoint, peak,
+                              midpoint, peak,
+                              destination.x, destination.y);
+
+    }
+    
+    // This is the layer that animates along the path. 
+    CALayer *layer = [CALayer layer];
+    
+    [layer setShadowColorUIColor blackColor].CGColor];
+    [layer setContents:(__bridge id)[UIImage imageNamed:@"american-flag.png"].CGImage];
+    [layer setBounds:CGRectMake(0.0, 0.0, 75.0, 75.0)];
+    [layer setPosition:CGPointMake(15.0, 15.0)];
+    [layer setShadowPathUIBezierPath bezierPathWithRectlayer bounds]].CGPath];
+    [layer setShadowOpacity:0.8];
+    [layer setShadowOffset:CGSizeMake(5.0, 5.0)];
+    
+    [[[self view] layer] addSublayer:layer];
+    
+    [self addPathAnimationToLayer:layer shouldRepeat:YES];
+    
+    // Set the view layer's delegate to the view controller. The delegate simply draws the path.
+    [[[self view] layer] setContentsScale[UIScreen mainScreen] scale]];
+    [[[self view] layer] setDelegate:self];
+    [[[self view] layer] setNeedsDisplay];
